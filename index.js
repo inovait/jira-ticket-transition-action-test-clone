0fb094981cc686da558152acfd174088bd4dc85a
Peter Muraus <peter.muraus@inova.si>
2022-11-11 13:13:04 +0100

    test

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const action = require("./main")
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-let pullRequestRef = ""
-if (github.context.payload.pull_request && github.context.payload.pull_request.head && github.context.payload.pull_request.head.ref) {
+
+async function run() {
+  let pullRequestRef = ""
+  if (github.context.payload.pull_request && github.context.payload.pull_request.head && github.context.payload.pull_request.head.ref) {
     pullRequestRef = github.context.payload.pull_request.head.ref
+  }
+  console.log(JSON.stringify(github.context))
+  console.log(JSON.stringify(github.context.payload))
+  let before = github.context.payload.before
+  let after = github.context.payload.after
+  let commitTickets = await action.extractCommits(after, before)
+  let ticketList = [...commitTickets, pullRequestRef, github.context.payload.ref]
+  console.log(JSON.stringify(ticketList))
 }
-console.log(JSON.stringify(github.context))
-console.log(JSON.stringify(github.context.payload))
-let before = github.context.payload.before
-let after = github.context.payload.after
-let commitTickets = action.extractCommits(after, before)
-let ticketList = [...commitTickets, pullRequestRef, github.context.payload.ref]
-console.log(JSON.stringify(ticketList))
+
+run()
