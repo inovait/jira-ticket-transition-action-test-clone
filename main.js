026e0b40d4e0ed5f2942e35f9e954fcc1f84eec7
Peter Muraus <peter.muraus@inova.si>
2022-11-11 13:17:17 +0100

    test

--- main.js
+++ main.js
@@ -63,8 +63,7 @@ async function transitionTickets(tickets, sourceTransition, targetTransition, me
 
 async function extractCommits(after, before) {
   let options = {
-    from: after,
-    to: before
+    from: after
   }
   if (options.from === "0000000000000000000000000000000000000000") {
     delete options.from
